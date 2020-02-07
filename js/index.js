d1911b444f4052206f3afdb26581cd4e9d71491c
Hank <3490170+Hank076@users.noreply.github.com>
2020-02-07 20:42:23 +0800

    新增緊急通知

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    Swal.fire({
+        title: '緊急通知',
+        text: '如果你曾於1/31前往北北基地區幾個重要景點，例如基隆廟口、九份、台北101、西門町等，請進行自主健康管理，並留意個人是否有發燒或呼吸道症狀。',
+        icon: 'warning',
+        confirmButtonText: '確定',
+        footer: '詳細地點：<a target="_blank" href="http://bit.ly/2SpSxeT">http://bit.ly/2SpSxeT</a>'
+    });
+});
+
