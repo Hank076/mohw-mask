ee068bf057a0f06047485b25bdb0cf3ecf8e091a
Pupu(DotNet) <dotnetchen@unidyna.com>
2020-02-09 11:18:33 +0800

    1. 調整圖資樣式 2. 新增過濾功能 3. 優化訊息顯示方式

--- js/index.js
+++ js/index.js
@@ -1,12 +1,60 @@
 $(function() {
     $("#news").click(function(event) {
+        showWarningMassage();
+    });
+
+    $("#info").click(function(event) {
         Swal.fire({
-            title: '緊急通知',
-            text: '如果你曾於1/31前往北北基地區幾個重要景點，例如基隆廟口、九份、台北101、西門町等，請進行自主健康管理，並留意個人是否有發燒或呼吸道症狀。',
-            icon: 'warning',
-            confirmButtonText: '確定',
-            footer: '詳細地點：<a target="_blank" href="http://bit.ly/2SpSxeT">http://bit.ly/2SpSxeT</a>'
+            title: '提醒',
+            html: '❕部分藥局因採發放號碼牌方式，方便民眾購買口罩，系統目前無法顯示已發送號碼牌數量。<br /><br />❕口罩數量以藥局實際存量為主，線上查詢之數量僅供參考。 ',
+            icon: 'info',
+            confirmButtonText: '確定'
         });
+    });
+
+    $("#filter").click(function(event) {
+        $(".filter_ctl").attr('disabled', true);
+
+        dont_show_null_inventory = !$("#show_null_inventory").is(":checked");
+        var type = $("input[name='type']:checked").val();
+
+        if(type == '0'){
+            //不過濾
+            show_adult_inventory = false;
+            show_child_inventory = false;
+
+        }else if(type == '1'){
+            //只顯示成人 > 0
+            show_adult_inventory = true;
+            show_child_inventory = false;
+
+        }else if(type == '2'){
+            //只顯示兒童 > 0
+            show_adult_inventory = false;
+            show_child_inventory = true;
+            
+        }else if(type == '3'){
+            //顯示成人+兒童 > 0
+            show_adult_inventory = true;
+            show_child_inventory = true;
+        }
+
+        //清除 markers
+        markers.clearLayers();
+
+        //更新地圖
+        createStrongholdData();
+
+        $(".filter_ctl").attr('disabled', false);
     });  
 });
 
+function showWarningMassage(){
+    Swal.fire({
+        title: '緊急通知',
+        text: '如果你曾於1/31前往北北基地區幾個重要景點，例如基隆廟口、九份、台北101、西門町等，請進行自主健康管理，並留意個人是否有發燒或呼吸道症狀。',
+        icon: 'warning',
+        confirmButtonText: '確定',
+        footer: '詳細地點：<a target="_blank" href="http://bit.ly/2SpSxeT">http://bit.ly/2SpSxeT</a>'
+    });
+}
