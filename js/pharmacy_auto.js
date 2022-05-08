246190c6c584a4813364fdbd114ee18dc7853c44
Pupu(DotNet) <pupu721@gmail.com>
2022-05-08 12:30:07 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -4414,6 +4414,7 @@ locations = [
 {"lng":"121.217313","lat":"24.862504","name":"佳欣社區藥局","id":"5932099856","tel":"034808717","addr":"桃園市龍潭區中正路262號1樓","business_week":"OOOOOOOOOOOOOOOOOOOXX"},
 {"lng":"121.2313","lat":"24.87337","name":"大心聯合藥局","id":"5932099927","tel":"034897998","addr":"桃園市龍潭區中興路390號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩實名制：週一至週日下午15:30開始販售"},
 {"lng":"121.243521","lat":"24.831225","name":"康是美藥局龍潭店","id":"5932099936","tel":"034991093","addr":"桃園市龍潭區中正路180、182號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
+{"lng":"121.221185","lat":"24.853822","name":"新資生龍潭中正大藥局","id":"5932099954","tel":"033606180","addr":"桃園市龍潭區中正路140—1號1樓","business_week":"XXXXXXXOOOOOOOXXXXXXX","memo":"下午2時開始售完為止"},
 {"lng":"121.217431","lat":"24.868098","name":"龍潭北龍藥局","id":"5932099963","tel":"034893123","addr":"桃園市龍潭區北龍路275號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩實名制，每天16:00開始販售，售完為止。"},
 {"lng":"121.217246","lat":"24.86388","name":"全球龍正藥局","id":"5932099972","tel":"034899639","addr":"桃園市龍潭區中正路231號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"營業時間:9:00-22:00"},
 {"lng":"121.211586","lat":"24.910336","name":"福澤中豐藥局","id":"5932100210","tel":"034283805","addr":"桃園市平鎮區中豐路345號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"家用快篩販賣時段15:30至售完為止"},
