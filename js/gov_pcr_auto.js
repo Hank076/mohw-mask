d13c7c834da3d1d805240729a2942615733d40b5
Hank <3490170+Hank076@users.noreply.github.com>
2022-07-02 16:59:22 +0800

    更新 maskdata_auto.csv & test_kits_auto.csv & pcr_auto.csv (from auto)

--- js/gov_pcr_auto.js
+++ js/gov_pcr_auto.js
@@ -373,7 +373,7 @@ gov_pcr_locations = [
 {"lng":"120.954953","lat":"24.651886","name":"三灣診所","id":"3535161046","tel":"037834233","addr":"苗栗縣三灣鄉中正路91號","business_week":"OOOOOOXOOOOOXXXXXXXXX"},
 {"lng":"120.82458","lat":"24.568981","name":"天恩診所","id":"3535012011","tel":"037277295","addr":"苗栗縣苗栗市中正路39號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"2\/26正常門診；2\/27上午診；2\/28正常門診"},
 {"lng":"120.619424","lat":"24.347256","name":"力倫診所","id":"3536031832","tel":"0426805889","addr":"台中市大甲區蔣公路246號","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
-{"lng":"120.681089","lat":"24.162638","name":"詠康耳鼻喉科診所","id":"3503260770","tel":"0422352015","addr":"台中市北區學士路312號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"6／4休診"},
+{"lng":"120.681089","lat":"24.162638","name":"詠康耳鼻喉科診所","id":"3503260770","tel":"0422352015","addr":"台中市北區學士路312號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"7／8~7／10休診"},
 {"lng":"120.645563","lat":"24.170529","name":"祥威耳鼻喉科診所","id":"3503271915","tel":"0427000132","addr":"台中市西屯區青海路二段240-8號","business_week":"OXOXXOOOXXXOXXXXOOOXO","memo":"2\/26-2\/28全日看診"},
 {"lng":"120.6495592","lat":"24.1662212","name":"翁耳鼻喉科診所","id":"3517062960","tel":"0424526346","addr":"台中市西屯區青海路二段209-3號","business_week":"OOOXOOXOOOXOOXXXXXXXX","memo":"7月起門診時間 上午8:30-12:00 午晚3:30-7:30   晚休"},
 {"lng":"120.654302","lat":"24.164942","name":"林鴻任耳鼻喉科診所","id":"3517064286","tel":"0423111518","addr":"台中市西屯區青海路一段116號","business_week":"OOXOOXOOOXOOXXOOOOOXX","memo":"星期日早上門診: 08:30 ~ 12:00 6月3日~6月5日休診"},
