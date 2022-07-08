7c3e07deb099b53098cc9bb7d8873f25491b8b09
Pupu(DotNet) <pupu721@gmail.com>
2022-07-08 21:04:03 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -4694,7 +4694,7 @@ locations = [
 {"lng":"121.6263","lat":"23.99112","name":"全心藥局","id":"5945012836","tel":"038223488","addr":"花蓮縣花蓮市永興路1號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"固定星期一到六 早上08:00 開始領購快篩，售完為止"},
 {"lng":"121.5994","lat":"23.98438","name":"承祐藥局","id":"5945012863","tel":"038338261","addr":"花蓮縣花蓮市建國路266號","business_week":"OOOOOOOOOOOOOOOOOOOOX","memo":"販售時間："},
 {"lng":"121.5939","lat":"23.97447","name":"宇康藥局","id":"5945012872","tel":"038357790","addr":"花蓮縣花蓮市中華路397號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"周日公休"},
-{"lng":"121.6009","lat":"23.9737","name":"達榮藥局","id":"5945012890","tel":"038316020","addr":"花蓮縣花蓮市中福路194號","business_week":"OOOOOOXOOOOOXXOOOOOOX","memo":"星期一到六 早上8:00-10:00 快篩販售"},
+{"lng":"121.6009","lat":"23.9737","name":"達榮藥局","id":"5945012890","tel":"038316020","addr":"花蓮縣花蓮市中福路185號","business_week":"OOOOOOXOOOOOXXOOOOOOX","memo":"星期一到六 早上8:00-10:00 快篩販售"},
 {"lng":"121.599","lat":"23.99447","name":"東大藥局","id":"5945012970","tel":"038463231","addr":"花蓮縣花蓮市富吉路4號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"實名制快篩販賣時間：早上10；00至售完為止"},
 {"lng":"121.5987","lat":"23.97165","name":"幸福花蓮藥局","id":"5945013048","tel":"038361412","addr":"花蓮縣花蓮市中正路179號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩販售時間週一至週六下午一點開賣晚9點30結束*周日彈性販售."},
 {"lng":"121.5971","lat":"23.99707","name":"富強健康藥局","id":"5945013057","tel":"038571839","addr":"花蓮縣花蓮市國裕里22鄰富強路128號","business_week":"OOOOOOOXXXXXXXOOOOOOO","memo":"營業時間:08:30~13:00／17:30~21:30公休日:另行通知。"},
