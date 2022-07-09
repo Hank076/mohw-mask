aa0996b0e79a0b7a796570e58c7462d8945696c0
Pupu(DotNet) <pupu721@gmail.com>
2022-07-09 11:30:05 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -515,6 +515,7 @@ locations = [
 {"lng":"121.5578","lat":"25.03488","name":"杏一光復藥局","id":"5901173925","tel":"0287804626","addr":"臺北市信義區光復南路449之1號","business_week":"XXXXXXXOOOOOOOOXOXOXX"},
 {"lng":"121.5577","lat":"25.03682","name":"躍獅光復藥局","id":"5901173934","tel":"0287862307","addr":"臺北市信義區光復南路417之8號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"下午1點開始販售"},
 {"lng":"121.5802","lat":"25.04133","name":"北一忠孝藥局","id":"5901173961","tel":"0223464588","addr":"臺北市信義區忠孝東路5段524巷1弄16號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"實名制快篩一~六販早上10點販售，周日不販售"},
+{"lng":"121.561798","lat":"25.029475","name":"吳興春天藥局","id":"5901174002","tel":"0227585618","addr":"臺北市信義區吳興街137號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"下午2:40發號碼牌"},
 {"lng":"121.5771","lat":"25.04112","name":"寶春藥局","id":"5901174011","tel":"0227566501","addr":"臺北市信義區虎林街119之19號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.5691","lat":"25.02454","name":"日安藥局","id":"5901174039","tel":"0227203775","addr":"臺北市信義區松仁路240巷13號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"販售實名制快篩請自備五百元不找零，每天上午十點半開始販售。"},
 {"lng":"121.5778","lat":"25.0389","name":"詠晴中西藥局","id":"5901174057","tel":"0227266890","addr":"臺北市信義區松山路484之4號(1樓含夾層)","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"實名制快篩 -現貨供應中.可直接攜帶健保卡來購買"},
