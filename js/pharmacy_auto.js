8a95606b91b1f8aad763dbc756b4c33cde713ef5
Pupu(DotNet) <pupu721@gmail.com>
2022-08-08 19:00:05 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -72,6 +72,7 @@ locations = [
 {"lng":"121.5473","lat":"25.04914","name":"傑安藥局","id":"5901010165","tel":"0227524155","addr":"臺北市松山區八德路2段437巷7弄8號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"9:30開始發號碼牌"},
 {"lng":"121.5466","lat":"25.05948","name":"仁弘藥局","id":"5901010174","tel":"0227136764","addr":"臺北市松山區民生東路3段113巷25弄6號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"早上9點開始發放快篩."},
 {"lng":"121.5615","lat":"25.05827","name":"益華藥局","id":"5901010192","tel":"0227624898","addr":"臺北市松山區民生東路5段138巷6號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
+{"lng":"121.5667","lat":"25.06178","name":"德靈藥局","id":"5901010272","tel":"0227646998","addr":"臺北市松山區富錦街548號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"週日休"},
 {"lng":"121.5449","lat":"25.06327","name":"健全藥局","id":"5901010290","tel":"0227128293","addr":"臺北市松山區民權東路3段103巷20號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.5449","lat":"25.06298","name":"瑞昌大藥局","id":"5901010496","tel":"0227151881","addr":"臺北市松山區民權東路3段103巷10號","business_week":"OOOOOOOOOOOOOXOOOOOOX"},
 {"lng":"121.5607","lat":"25.05364","name":"合生藥局","id":"5901010576","tel":"0227602932","addr":"臺北市松山區南京東路5段123巷45號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOX","memo":"8／6-8／7 公休"},
