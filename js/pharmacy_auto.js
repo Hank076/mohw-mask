bfa6b521bedaae72b80ac7e59d32aae146b83400
Pupu(DotNet) <pupu721@gmail.com>
2022-08-29 10:34:03 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -316,6 +316,7 @@ locations = [
 {"lng":"121.522022","lat":"25.081072","name":"資生堂藥局","id":"5901152079","tel":"0228857588","addr":"臺北市士林區承德路4段40巷17號1F","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
 {"lng":"121.5087","lat":"25.08844","name":"安泰藥師藥局","id":"5901152293","tel":"0228166977","addr":"臺北市士林區延平北路六段83號(實際使用:1、2樓)","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"星期一~星期六 下午2點30分 開始發快篩~"},
 {"lng":"121.5267","lat":"25.08878","name":"得安藥局","id":"5901152471","tel":"0228836336","addr":"臺北市士林區文林路140號","business_week":"XXXXXXXOOOOOOOOOOOOOO","memo":"星期一三五六下午3:30  星期二 四日下午5:00"},
+{"lng":"121.5083","lat":"25.08769","name":"仁大藥局","id":"5901152640","tel":"0288113793","addr":"臺北市士林區社子街13號1樓","business_week":"OOOOOOXOOOOOXXXXXXXXX","memo":"週一至週六上午8:40開始販售，週日公休。"},
 {"lng":"121.522772","lat":"25.084438","name":"和樹藥局","id":"5901152659","tel":"0228821188","addr":"臺北市士林區後港街45巷1號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"週日公休，周一早上9:30"},
 {"lng":"121.5088","lat":"25.08788","name":"博信藥局","id":"5901152720","tel":"0228128295","addr":"臺北市士林區延平北路6段61號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"星期天休息"},
 {"lng":"121.508","lat":"25.0875","name":"芳圓藥局","id":"5901152739","tel":"0228168666","addr":"臺北市士林區社子街26-1號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"快篩販售時間，目前量能足夠，早上9點15分開賣"},
