4d9dc6c8b7531a9a587b9ac14ed940635a0d63dd
Hank <3490170+Hank076@users.noreply.github.com>
2022-10-04 16:59:22 +0800

    更新 maskdata_auto.csv & test_kits_auto.csv & pcr_auto.csv (from auto)

--- js/gov_pcr_auto.js
+++ js/gov_pcr_auto.js
@@ -60,6 +60,7 @@ gov_pcr_locations = [
 {"lng":"121.2234105","lat":"24.95421439","name":"新國民醫療社團法人新國民醫院","id":"0932020034","tel":"034225180","addr":"桃園市中壢區復興路152號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"請以本院官方網站公告為準(http:\/\/www.skmh.com.tw\/)"},
 {"lng":"121.3186802","lat":"24.98410931","name":"德仁醫院","id":"1532011163","tel":"033617985","addr":"桃園市桃園區桃鶯路245號","business_week":"OOOOOOXOOOOOXXOOOOOXX"},
 {"lng":"121.3097107","lat":"24.98927235","name":"聯新國際醫院桃新分院","id":"1532010120","tel":"033325678","addr":"桃園市桃園區復興路195號","business_week":"OOOOOOXOOOOOOXOOOOOXX","memo":"星期六看診時間8:00~15:30"},
+{"lng":"121.238244","lat":"25.08140568","name":"聯新國際醫院桃園國際機場醫療中心","id":"3432060513","tel":"033983456","addr":"桃園市大園區航站南路9號、15號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"平日夜間20:30-08:30、假日及國定例假日提供急診服務"},
 {"lng":"121.3510651","lat":"24.81512769","name":"復興區衛生所","id":"2332130017","tel":"033822325","addr":"桃園市復興區澤仁里中正路25號","business_week":"OOOOOXXOOOOOXXXXXXXXX"},
 {"lng":"120.961966","lat":"24.816144","name":"國軍桃園總醫院新竹分院附設民眾診療服務處","id":"0512040014","tel":"035348181#325117","addr":"新竹市北區武陵路3號","business_week":"OOOOOOXOOOOOXXOOOOOXX"},
 {"lng":"120.965227","lat":"24.798257","name":"國泰醫療財團法人新竹國泰綜合醫院","id":"1112010528","tel":"035278999#7999","addr":"新竹市東區福德里中華路二段六七八號及六七八號之一","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"提供24小時急診服務。"},
