2682f7d413fe4aad304358bb66c5ba6e0edbdc6b
Pupu(DotNet) <pupu721@gmail.com>
2022-10-05 12:34:04 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -3053,6 +3053,7 @@ locations = [
 {"lng":"121.214","lat":"24.94629","name":"晨安藥局","id":"5932108305","tel":"034925202","addr":"桃園市平鎮區振興西路99號","business_week":"OOOOOOOOOOOOOOOOOOOXX","memo":"販售時段:早上8:00..... 22:00販賣"},
 {"lng":"121.2229","lat":"24.94747","name":"建德藥局","id":"5932108430","tel":"034686662","addr":"桃園市平鎮區北勢里金陵路103號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"販售時段:上午8:30 ，（周日休息）"},
 {"lng":"121.2218","lat":"24.9452","name":"大益藥局","id":"5932108529","tel":"034578962","addr":"桃園市平鎮區金陵路224號","business_week":"OOOOOOXOOOOOXXXXXXXXX","memo":"口罩販賣時段:早上8:30起至售完為止."},
+{"lng":"121.2032","lat":"24.95127","name":"天健示範藥局","id":"5932108574","tel":"034937169","addr":"桃園市平鎮區文化街252號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩實名制每天9點販售，售完為止"},
 {"lng":"121.218","lat":"24.9317","name":"鼎祥藥局","id":"5932109857","tel":"034679124","addr":"桃園市平鎮區關爺北路四二號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"實名制快篩早上9:00開始販售"},
 {"lng":"121.2139","lat":"24.9538","name":"優達藥局","id":"5932109955","tel":"034943488","addr":"桃園市平鎮區廣明路六六號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"快篩9點賣"},
 {"lng":"121.2163","lat":"24.94219","name":"新英藥局","id":"5932109973","tel":"034687729","addr":"桃園市平鎮區中豐路208號1樓","business_week":"OOOOOOXOOXOOXXOOOOOXX","memo":"快篩銷售時段:  營業時段內均有銷售 祝大家平安健康"},
