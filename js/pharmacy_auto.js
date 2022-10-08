b0d758dfe0cfd43ca62a65b5e1755a6a2f65960b
Pupu(DotNet) <pupu721@gmail.com>
2022-10-08 17:00:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -2543,6 +2543,7 @@ locations = [
 {"lng":"121.8069","lat":"25.1076","name":"揚明藥局","id":"5931121304","tel":"0224970925","addr":"新北市瑞芳區民族街2號1樓","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"快篩代售時間自08:00~18:30"},
 {"lng":"121.8066","lat":"25.10814","name":"瑞光藥局","id":"5931121386","tel":"0224972070","addr":"新北市瑞芳區明燈路3段70號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOX"},
 {"lng":"121.8055","lat":"25.10824","name":"長興堂藥局","id":"5931121411","tel":"0224972050","addr":"新北市瑞芳區明燈路3段69號1樓","business_week":"OOOOOOOOOOOOXXOOOOOXX"},
+{"lng":"121.8057","lat":"25.10812","name":"瑞芳藥局","id":"5931121466","tel":"0224966588","addr":"新北市瑞芳區明燈路3段59之1及59之2號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩實名制，每天16:00開始販售，售完為止。"},
 {"lng":"121.4463","lat":"24.98538","name":"博惠藥局","id":"5931130152","tel":"0222618337","addr":"新北市土城區中央路1段249巷20號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"14:00開始販售政府快篩，不發號碼牌"},
 {"lng":"121.4626","lat":"24.98825","name":"崇愛藥局","id":"5931130214","tel":"0222665506","addr":"新北市土城區永豐路17號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.4391","lat":"24.96977","name":"久達藥局","id":"5931130223","tel":"0222696982","addr":"新北市土城區中央路3段47號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"一至六早上8點營業開賣快篩"},
