8324362f6bd413ef6406908656b8c5b1e331f981
Pupu(DotNet) <pupu721@gmail.com>
2022-11-22 20:30:05 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -2537,6 +2537,7 @@ locations = [
 {"lng":"121.4637","lat":"24.98557","name":"大眾藥局","id":"5931132736","tel":"0222666057","addr":"新北市土城區明峰街6巷1號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"販售狀況依門市公告為主"},
 {"lng":"121.4522","lat":"24.98595","name":"德隆藥局","id":"5931132781","tel":"0222656056","addr":"新北市土城區裕民路153號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"依實際庫存代售快篩試劑"},
 {"lng":"121.4407","lat":"24.97312","name":"健業藥局","id":"5931132807","tel":"0222704798","addr":"新北市土城區中央路2段223-10號","business_week":"OXOXOOOOXOXOOOOOOOOXX"},
+{"lng":"121.4566","lat":"24.98829","name":"大康藥局","id":"5931132816","tel":"0222738004","addr":"新北市土城區學府路1段72號2樓","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"實名制快篩早上8點開始販售"},
 {"lng":"121.4539","lat":"24.98661","name":"佳霖藥局","id":"5931132852","tel":"0222641425","addr":"新北市土城區學府路1段170號1樓","business_week":"OOOOOOXOOOOOXXOOOOOXX"},
 {"lng":"121.4664","lat":"24.98465","name":"美康藥妝藥局","id":"5931132950","tel":"0222616868","addr":"新北市土城區延吉街254號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.458","lat":"24.98854","name":"學府松藥局","id":"5931133019","tel":"0282628665","addr":"新北市土城區學府路一段38號1、2樓","business_week":"OOOOOOXOOOOOOXOOOOOXX","memo":"實名制快篩:每日14:30-15:30販售.兒童0-6歲每日15:30-16:30兌換.國定假日例假日公休。"},
