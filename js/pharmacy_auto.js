3fff4184e6920f5967cc01446dae0f09cf31cf83
Pupu(DotNet) <pupu721@gmail.com>
2022-11-28 00:04:04 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -729,6 +729,7 @@ locations = [
 {"lng":"120.7307","lat":"24.24275","name":"昭仁藥局","id":"5903010341","tel":"0425243559","addr":"臺中市豐原區南陽路140號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"早上8:30販售"},
 {"lng":"120.733008","lat":"24.250931","name":"甲蒂健保藥局","id":"5903010350","tel":"0425256028","addr":"臺中市豐原區豐東路308號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"120.7219","lat":"24.2551","name":"豐原藥局","id":"5903010449","tel":"0425260119","addr":"臺中市豐原區新生北路206號之1(1樓)","business_week":"OOOOOOXXXXXXXXOXOXXXX","memo":"週一至週六上午8:30-11:30。週一下午4-8:30，週三晚7-8:30。"},
+{"lng":"120.714857","lat":"24.250094","name":"心之藥局","id":"5903010458","tel":"0425254959","addr":"臺中市豐原區中山路504號","business_week":"OOOOOXOOOOOOXOOOOOOXO","memo":"周一至周五 賣完為止"},
 {"lng":"120.7283","lat":"24.25084","name":"大樹圓環藥局","id":"5903010485","tel":"0425122212","addr":"臺中市豐原區圓環東路617號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"口罩販售時間：14:00 - 16:00"},
 {"lng":"120.727168","lat":"24.248238","name":"佑全豐原圓環東藥局","id":"5903010501","tel":"0425246069","addr":"臺中市豐原區圓環東路548、550、552、554號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"營業時間9:00-22:00，快篩購買時間09:00至21:30止"},
 {"lng":"120.7159","lat":"24.25142","name":"安德豐原藥局","id":"5903010510","tel":"0425200733","addr":"臺中市豐原區中正路295號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
