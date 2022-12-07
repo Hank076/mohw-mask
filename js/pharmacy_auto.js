6c002bedbc482c51965f5292ce743c92ef3abf48
Pupu(DotNet) <pupu721@gmail.com>
2022-12-07 09:34:04 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -1786,6 +1786,7 @@ locations = [
 {"lng":"120.1879","lat":"22.98173","name":"佑佑藥局","id":"5921021566","tel":"062912603","addr":"臺南市南區金華里健康路二段349號2F","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"口罩販售時間及營業時間"},
 {"lng":"120.212254","lat":"22.980223","name":"為仁藥師藥局","id":"5921021708","tel":"062139376","addr":"臺南市南區大同路二段106號","business_week":"OOOOOOOXXXXXOXOOOOOXX","memo":"實名制快篩試劑販售及免費兒童試劑發放於每日上午8:05開始"},
 {"lng":"120.1881","lat":"22.97514","name":"海山藥局","id":"5921021851","tel":"062617788","addr":"臺南市南區新興路400號","business_week":"OOOOOOOOOOOOOXOOOOOOX","memo":"實名制快篩免費領取（6歲以下&65歲以上）與代售（每人最多可購買6份）"},
+{"lng":"120.1954","lat":"22.98933","name":"三芳藥局","id":"5921030092","tel":"062203084","addr":"臺南市中西區元安里大勇街5號","business_week":"OOOOOOXOOOOOXXXXXXXXX","memo":"週一至週六09:00~12:00  14:30~17:00執業，快篩賣完為止。"},
 {"lng":"120.1924","lat":"22.9999","name":"雅里藥局","id":"5921030118","tel":"062200881","addr":"臺南市中西區民族路三段285號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"實名制快篩販售:周一至周日 上午08:30~售完  請加LINE諮詢@neo1819x"},
 {"lng":"120.1942","lat":"22.9898","name":"星宗藥局","id":"5921030289","tel":"062223453","addr":"臺南市中西區新安里大勇街37號","business_week":"OOOOOOOOOOOOOXOOOOOOX","memo":"11:00開始販售"},
 {"lng":"120.1776","lat":"23.00011","name":"銘豐藥局","id":"5921030298","tel":"062592517","addr":"臺南市中西區西賢一街45號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"星期一-日09:00-22:00.快篩實名制每日發放"},
