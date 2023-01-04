95396d64130754e60f0997a14131f3d68c79f413
Pupu(DotNet) <pupu721@gmail.com>
2023-01-04 13:34:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -1353,7 +1353,7 @@ locations = [
 {"lng":"121.7962","lat":"25.13991","name":"文裕藥局","id":"5911011450","tel":"0224691239","addr":"基隆市中正區八斗街10號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"星期一至星期六早上8:10開始販售，星期日時間於臉書公布"},
 {"lng":"121.7859","lat":"25.13568","name":"維元社區藥局","id":"5911011469","tel":"0224601388","addr":"基隆市中正區新豐街448號","business_week":"OOOOOOOOOOOOOXOOOOOOX"},
 {"lng":"121.7963","lat":"25.13946","name":"源昌藥局","id":"5911011496","tel":"0224691686","addr":"基隆市中正區北寧路384號1樓","business_week":"OOOOOOOOOOOOOXOOOOOOX"},
-{"lng":"121.7958","lat":"25.13944","name":"八斗子東亞藥局","id":"5911011512","tel":"0224698829","addr":"基隆市中正區北寧路378號","business_week":"OOOOOOOOOOOOOOOOOOOXX","memo":"中午12點鐘開賣實名制快篩，售完為止"},
+{"lng":"121.7958","lat":"25.13944","name":"八斗子東亞藥局","id":"5911011512","tel":"0224698829","addr":"基隆市中正區北寧路378號","business_week":"OOOOOOOOOOOOOOOOOOXXX","memo":"中午12點鐘開賣實名制快篩，售完為止"},
 {"lng":"121.7605","lat":"25.14233","name":"禎宸藥局","id":"5911011521","tel":"0224620366","addr":"基隆市中正區中正路175號1樓","business_week":"OOOOOOXOOOOOOXOOOOOXX"},
 {"lng":"121.7458","lat":"25.13279","name":"百福義一藥師藥局","id":"5911011576","tel":"0224268288","addr":"基隆市中正區義一路48號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"周一至周六08:00開門，周日09:00開門"},
 {"lng":"121.7895","lat":"25.138","name":"禾新藥局","id":"5911011594","tel":"0224698940","addr":"基隆市中正區新豐街205巷7號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩實名制販售時間:下午3點開始"},
