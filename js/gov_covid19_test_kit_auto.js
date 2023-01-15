537ef7ea11b58671afcd41b3788d58c8527dd9d7
Pupu(DotNet) <pupu721@gmail.com>
2023-01-15 18:00:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/gov_covid19_test_kit_auto.js
+++ js/gov_covid19_test_kit_auto.js
@@ -471,7 +471,7 @@ gov_covid19_test_kit_locations = [
 {"lng":"121.083378","lat":"24.743969","name":"崇光小兒專科診所","id":"3533031821","tel":"035959878","addr":"新竹縣竹東鎮長春路三段188號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOX","memo":"12／31上午開診下午開診晚上開診.1／1上午開診下午開診.1／2上午開診下午開診晚上開診"},
 {"lng":"121.00573","lat":"24.83239","name":"傅仁良小兒科診所","id":"3533051547","tel":"035530106","addr":"新竹縣竹北市博愛街172號","business_week":"OOOOOOOOOOOXXXOOOOOXX"},
 {"lng":"121.009949","lat":"24.825628","name":"游恆懿診所","id":"3533051636","tel":"035586721","addr":"新竹縣竹北市縣政八街23號","business_week":"OOOOOOXOXOXXXXXXXOXXX"},
-{"lng":"121.024942","lat":"24.822424","name":"啄木鳥耳鼻喉科診所","id":"3533052099","tel":"036586586","addr":"新竹縣竹北市莊敬三路39號1號","business_week":"OOOOOOOOOOOOOXOOOOOOX","memo":"12／31上午開診下午開診晚上開診.1／1上午開診晚上開診.1／2上午開診下午開診晚上開診"},
+{"lng":"121.024942","lat":"24.822424","name":"啄木鳥耳鼻喉科診所","id":"3533052099","tel":"036586586","addr":"新竹縣竹北市莊敬三路39號1號","business_week":"OOOOOOOOOOOOOXOOOOOOO","memo":"12／31上午開診下午開診晚上開診.1／1上午開診晚上開診.1／2上午開診下午開診晚上開診"},
 {"lng":"121.028431","lat":"24.814446","name":"艾兒診所","id":"3533052491","tel":"036586699","addr":"新竹縣竹北市福興東路二段140號","business_week":"OOOOOOXOOOOOOXOOOOOXX","memo":"12／31上午開診下午開診.1／2上午開診晚上開診"},
 {"lng":"121.044863","lat":"24.9026","name":"朱先營診所","id":"3533060153","tel":"035998325","addr":"新竹縣湖口鄉中正路一段27號","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
 {"lng":"121.115719","lat":"24.718969","name":"橫山庄診所","id":"3533071058","tel":"035931395","addr":"新竹縣橫山鄉橫山街一段216號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"無急診"},
