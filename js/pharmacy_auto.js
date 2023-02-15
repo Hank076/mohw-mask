fb9069649b2e15e606b6153cff6a7f732478f40d
Pupu(DotNet) <pupu721@gmail.com>
2023-02-15 16:34:08 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -2465,6 +2465,7 @@ locations = [
 {"lng":"121.4605","lat":"25.08169","name":"中美永結藥局","id":"5931143266","tel":"0222887676","addr":"新北市蘆洲區長安街185號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
 {"lng":"121.461116","lat":"25.093181","name":"大樹長榮藥局","id":"5931143319","tel":"0228480567","addr":"新北市蘆洲區長榮路61、61-1號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.4699","lat":"25.08364","name":"全家藥粧藥局","id":"5931143328","tel":"0222811215","addr":"新北市蘆洲區光華路4號1樓","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
+{"lng":"121.4793","lat":"25.08936","name":"昭陽藥局","id":"5931143355","tel":"0282867077","addr":"新北市蘆洲區復興路230號1樓","business_week":"XOOOOOOXOOOOOOXOOOOOO","memo":"可以領健保快篩試劑"},
 {"lng":"121.4734","lat":"25.08868","name":"光禾藥局","id":"5931143364","tel":"0228480050","addr":"新北市蘆洲區民權路122號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.4354","lat":"25.07308","name":"成泰藥局","id":"5931150065","tel":"0222924305","addr":"新北市五股區成泰路1段143號","business_week":"OOOOOOXOOOOOOOOOOOOOO"},
 {"lng":"121.4336","lat":"25.06674","name":"德泰藥局","id":"5931150163","tel":"0229095927","addr":"新北市五股區成泰路1段9號","business_week":"OXOXOOXOOOOOOXOOOOOXX"},
