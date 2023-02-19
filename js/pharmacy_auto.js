7cc81fddd0d48e3fbe3f630e1e57fc41888d16bd
Pupu(DotNet) <pupu721@gmail.com>
2023-02-19 19:30:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -221,6 +221,7 @@ locations = [
 {"lng":"121.5214","lat":"25.05143","name":"興康源藥局","id":"5901102677","tel":"0225311248","addr":"臺北市中山區中山北路1段140巷8號","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
 {"lng":"121.5426","lat":"25.04839","name":"正傑健保藥局","id":"5901102837","tel":"0227310181","addr":"臺北市中山區長安東路2段181號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"下午四點開始販售"},
 {"lng":"121.5493","lat":"25.08191","name":"保德明水藥局","id":"5901102971","tel":"0285097769","addr":"臺北市中山區北安路608巷4弄1號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"僅除夕營業到下午五點，其他都正常營業9:00-22:30，全年無休，可領／買快篩"},
+{"lng":"121.5486","lat":"25.08194","name":"大直健康藥局","id":"5901103110","tel":"0225323310","addr":"臺北市中山區北安路591號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"販售時間:W1-W7:09:00-21:00"},
 {"lng":"121.5349","lat":"25.05305","name":"娸淇藥局","id":"5901103147","tel":"0225082762","addr":"臺北市中山區伊通街95號","business_week":"OOOOOXXOOOOOXXOOOOOXX","memo":"口罩販售時間2:30PM(六日無發口罩)4\/2-4\/5 清明休息"},
 {"lng":"121.5435","lat":"25.05615","name":"OK藥師藥局","id":"5901103183","tel":"0225170068","addr":"臺北市中山區興安街95號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"全年無休"},
 {"lng":"121.5269","lat":"25.05018","name":"昶青林森藥局","id":"5901103192","tel":"0225639939","addr":"臺北市中山區林森北路119巷52號","business_week":"OOOOOXXOOOOOOOOOOOOOX","memo":"實名制快篩每日14:00開始販售，20:00結束或售完為止，週日人力不足不發售"},
