64c817b2e46598532e22d818181c5667cef9ed6c
Pupu(DotNet) <pupu721@gmail.com>
2023-02-21 11:04:04 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -2536,6 +2536,7 @@ locations = [
 {"lng":"121.2163","lat":"24.95532","name":"中壢中正藥局","id":"5932022631","tel":"034014100","addr":"桃園市中壢區中正路363、365號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.247932","lat":"24.97409","name":"得群藥局","id":"5932022971","tel":"034535953","addr":"桃園市中壢區自強六路42號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"快篩試劑販售時間為營業時間內皆可"},
 {"lng":"121.2108","lat":"24.95947","name":"美佑康活力藥局","id":"5932022980","tel":"032810018","addr":"桃園市中壢區五福里中正路627號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
+{"lng":"121.2625","lat":"24.95597","name":"鴻安中西藥局","id":"5932023012","tel":"034520615","addr":"桃園市中壢區新中北路二段191號1樓","business_week":"OOOOOOOXXXXXXXXXXXXXX","memo":"快篩1030開始。週一到周日都有。"},
 {"lng":"121.2585","lat":"24.96527","name":"辰鴻藥局","id":"5932023021","tel":"034638639","addr":"桃園市中壢區環中東路110號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"營業時間都有供應"},
 {"lng":"121.227467","lat":"24.949433","name":"福澤健行藥局","id":"5932023058","tel":"034681988","addr":"桃園市中壢區健行路139號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"家用快篩販賣時段15:30至售完為止"},
 {"lng":"121.217593","lat":"24.962847","name":"中壢民權藥局","id":"5932023094","tel":"034014961","addr":"桃園市中壢區民權路326、328號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"實聯制快篩每天78份週一至週六下午1350拿取號碼牌1400開始販售.依序入場販售.過號視同放棄"},
