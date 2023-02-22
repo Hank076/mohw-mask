f26d49351fd9b691fa655c17a4df0bbf71f130af
Pupu(DotNet) <pupu721@gmail.com>
2023-02-22 11:04:07 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -2270,6 +2270,7 @@ locations = [
 {"lng":"121.4619","lat":"25.12951","name":"憲立藥局","id":"5931102309","tel":"0228092212","addr":"新北市淡水區自強路295號1樓","business_week":"OOOOOOXOOOOOOXOOOOOXX","memo":"快篩星期一至六 早上09:30發放號碼牌 現場等待領取"},
 {"lng":"121.4456","lat":"25.17008","name":"淡大郭藥局","id":"5931102336","tel":"0226229966","addr":"新北市淡水區英專路32號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"9:00-21:00"},
 {"lng":"121.4388","lat":"25.19728","name":"日森人文藥局淡海店","id":"5931102425","tel":"0226221313","addr":"新北市淡水區新市五路3段386號1樓","business_week":"XXXXXOXOOOOOXXOOOOOOX"},
+{"lng":"121.4471","lat":"25.18403","name":"西雅圖聯合藥局","id":"5931102434","tel":"0226200910","addr":"新北市淡水區新市一路3段101巷16弄20號1樓","business_week":"XXXXXXXOOOOOOXOOOOOOX"},
 {"lng":"121.4443","lat":"25.18344","name":"池慶藥局","id":"5931102452","tel":"0226238852","addr":"新北市淡水區中山北路2段5號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.447","lat":"25.172","name":"水源藥局","id":"5931102489","tel":"0226295233","addr":"新北市淡水區水源街1段133號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"121.642115","lat":"25.065604","name":"麒豐藥局","id":"5931110034","tel":"0226434737","addr":"新北市汐止區樟樹一路135巷2號","business_week":"OOOOOOOOOOOOOXOOOOOOX","memo":"實名制口罩銷售時間9:30~21:00"},
