6f9b28acbc38a9b45f20e3ecae83d0b55895578c
Pupu(DotNet) <pupu721@gmail.com>
2023-02-23 09:34:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -3924,6 +3924,7 @@ locations = [
 {"lng":"120.528085","lat":"22.652774","name":"泰安藥局","id":"5943070027","tel":"087221543","addr":"屏東縣麟洛鄉麟頂村民生路26號","business_week":"OOOOOOOOOOOOOOOOOOOOX","memo":"7:30--21:00  7:30開始"},
 {"lng":"120.524681","lat":"22.652512","name":"立登藥局","id":"5943071088","tel":"087213590","addr":"屏東縣麟洛鄉中山路256之1號","business_week":"OOOOOOOOOOOOOOOOOOOOX","memo":"星期一到星期日早上8點開始販售"},
 {"lng":"120.4888","lat":"22.73183","name":"穎昇藥局","id":"5943081146","tel":"087393672","addr":"屏東縣九如鄉九如路二段131號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"販賣實名制快篩時間，中午12點開始至售完為止。"},
+{"lng":"120.4888","lat":"22.7316","name":"信昌藥局","id":"5943081155","tel":"087397337","addr":"屏東縣九如鄉九如路2段119號","business_week":"OOOOOOOOOOOOOOOOOOOOX","memo":"自5／6起實名制快篩販賣時間:早上8:30開始.售完為止"},
 {"lng":"120.4893","lat":"22.73311","name":"九如藥局","id":"5943081164","tel":"087397777","addr":"屏東縣九如鄉九如路二段230號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"120.490545","lat":"22.776346","name":"祥安藥局","id":"5943091151","tel":"087757652","addr":"屏東縣里港鄉玉田村玉田路63-2號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
 {"lng":"120.4961","lat":"22.77736","name":"葉藥局里港店","id":"5943091197","tel":"087750318","addr":"屏東縣里港鄉春林村永春路17-5號1樓","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"位於永春路車牌旁.土地公廟斜對面（早上8點領快篩）現場領.無號碼牌（售完為止）"},
