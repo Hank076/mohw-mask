d04a83fee155dc4d6041f935b4bc9455c9aaa742
Pupu(DotNet) <pupu721@gmail.com>
2023-02-27 17:30:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -3686,6 +3686,7 @@ locations = [
 {"lng":"120.3389","lat":"22.61296","name":"順發藥局","id":"5942013902","tel":"077630666","addr":"高雄市鳳山區新強里新富路239號","business_week":"OOOOOOOOOOOOXOOOOOOXO","memo":"每週日不販售實名制快篩，其餘開診時段皆有販售"},
 {"lng":"120.2925","lat":"22.79481","name":"惠光西藥局","id":"5942020050","tel":"076212163","addr":"高雄市岡山區維仁路147號","business_week":"OOOOOOXOOOOOOXOOOOOOX"},
 {"lng":"120.2845","lat":"22.78459","name":"協安大藥局","id":"5942020078","tel":"076252256","addr":"高雄市岡山區介壽西路10號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"上午8點30分開始販售"},
+{"lng":"120.301646","lat":"22.805299","name":"建成藥局","id":"5942020130","tel":"076210458","addr":"高雄市岡山區台上里大仁路97號","business_week":"OOOOOOXOOOOOOXOOOOOOX","memo":"營業日.販賣快篩劑.時間9.00-21.30.或至售完為止"},
 {"lng":"120.2961","lat":"22.79292","name":"平和藥局","id":"5942020176","tel":"076212592","addr":"高雄市岡山區岡山路268號","business_week":"OOOOOOOOOOOOOXOOOOOOX","memo":"實名制快篩，周一~周五早上8點開始販售，售完為止"},
 {"lng":"120.2882","lat":"22.79476","name":"鴻興大藥局","id":"5942020185","tel":"076259903","addr":"高雄市岡山區中華路136號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"有營業  就有賣快篩。"},
 {"lng":"120.3018","lat":"22.80071","name":"建元藥局","id":"5942020265","tel":"076216672","addr":"高雄市岡山區大仁北路104號","business_week":"OOOOOXXOOOOOXXOOOOOXX","memo":"早上8點開始販售，售完為止。"},
