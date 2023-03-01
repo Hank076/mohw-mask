ca67e2e13e9a684b9c56e6c4fd77626e310e98da
Pupu(DotNet) <pupu721@gmail.com>
2023-03-01 13:30:06 +0800

    更新 pharmacy_auto.js, gov_pcr_auto.js, gov_covid19_test_kit_auto.js (from auto)

--- js/pharmacy_auto.js
+++ js/pharmacy_auto.js
@@ -3006,6 +3006,7 @@ locations = [
 {"lng":"120.368923","lat":"23.899301","name":"二林貓頭鷹藥局","id":"5937081578","tel":"048952299","addr":"彰化縣二林鎮中正路24、26號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"營業時間 發放號碼牌"},
 {"lng":"120.3667","lat":"23.8998","name":"嘉信藥局","id":"5937081587","tel":"048954029","addr":"彰化縣二林鎮中山路140號","business_week":"OOOOOOOOOOOOOOOOOOOOO","memo":"每天早上9點開始，賣完為止"},
 {"lng":"120.3709","lat":"23.89951","name":"二林藥局","id":"5937081596","tel":"048950010","addr":"彰化縣二林鎮斗苑路五段39號","business_week":"OOOOOOOOOOOOOOOOOOOOO"},
+{"lng":"120.4709","lat":"24.13103","name":"線西藥局","id":"5937091056","tel":"047582482","addr":"彰化縣線西鄉寓埔村中央路二段48號","business_week":"OOOOOXXOOOOOXXXXXXXXX","memo":"實名制快篩試劑 週一至週五 8:30-17:30 週六週日公休 週間遇例假日公休"},
 {"lng":"120.463043","lat":"24.134462","name":"寓埔藥局","id":"5937091065","tel":"047589805","addr":"彰化縣線西鄉寓埔村中華路48號1樓","business_week":"OOOOOXXOXOOOXXOOOOOXO","memo":"快篩販賣時間:週六 週日公休"},
 {"lng":"120.4859","lat":"24.14825","name":"伸港藥局","id":"5937101146","tel":"047992663","addr":"彰化縣伸港鄉新港村信義路135號","business_week":"OOOOOOOOOOOOOOOOOOOXX"},
 {"lng":"120.4885","lat":"24.14758","name":"貓頭鷹藥局","id":"5937101173","tel":"047996596","addr":"彰化縣伸港鄉新港村中山東路99號","business_week":"OOOOOOXOOOOOXXOOOOOXX","memo":"周一至周六早上 8:00 開始販售實名制快篩。收受：牙科、診所衛生所、各大醫院慢性病連續處方箋，免費領藥。"},
